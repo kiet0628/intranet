91f5e3627ba981154c8fcb0dfead9c30e8f281fb
Cloud User <ec2-user@ip-172-31-52-206.ec2.internal>
2023-05-17 13:55:05 +0000

    using wiki.js

--- scripts/installEnv.sh
+++ scripts/installEnv.sh
@@ -6,5 +6,7 @@ source bash_profile
 cp $PRJ/scripts/bash_profile ~/.bash_profile
 source ~/.bash_profile
 
+sudo yum update
+
 echo "Completed Install Env."
 
--- scripts/installSw.sh
+++ scripts/installSw.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
 
 echo "Install SW."
-export TMP=$PRJ/tmp
+export DOWNLOADS=$PRJ/downloads
 
-mkdir -p $TMP
+mkdir -p $DOWNLOADS
 
-pushd $TMP
-  if [ ! -f "apache-tomcat-9.0.75.tar.gz" ]; then
-	wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.75/bin/apache-tomcat-9.0.75.tar.gz
-  fi
-  if [ ! -f "JSPWiki.war" ]; then
-	wget https://downloads.apache.org/jspwiki/2.11.3/binaries/webapp/JSPWiki.war
+pushd $DOWNLOADS
+  if [ ! -f "wiki-js.tar.gz" ]; then
+	wget https://github.com/Requarks/wiki/releases/latest/download/wiki-js.tar.gz
   fi
 popd
 
 pushd $PRJ
-	
-	tar -xvf $TMP/apache-tomcat-9.0.75.tar.gz
-	rm -rf $TC/webapps/docs
-	rm -rf $TC/webapps/examples
-	rm -rf $TC/webapps/host-*
-	rm -rf $TC/webapps/manager
-	rm -rf $TC/webapps/ROOT
-
-	cp $TMP/JSPWiki.war $TC/webapps/ROOT.war
+	rm -rf wiki
+	mkdir -p wiki	
+	cd wiki
+	tar -xvf $DOWNLOADS/wiki-js.tar.gz
+	cp $PRJ/config/config.* wiki
 popd
 
 
-echo "Completed Install SW."
+echo ".................Completed Install SW."
 
