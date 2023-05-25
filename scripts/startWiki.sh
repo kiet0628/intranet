2860cc278d1edee9ecf410afa53a983e1f5b6d4d
Cloud User <ec2-user@ip-172-31-3-0.ec2.internal>
2023-05-25 16:59:01 +0000

    add auto startup

--- scripts/startWiki.sh
+++ scripts/startWiki.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
+# crontab -e
+# @reboot date >> ~/rebootDate.txt;  cd /data/intranet/scripts/; ./startWiki.sh > /data/intranet/wiki/wiki.log
 
+. /home/ec2-user/.bash_profile
 
 pushd $PRJ/scripts
 	./stopWiki.sh
 popd 
+sleep 1
+
 
 echo ".....Starting node server"
 pushd $PRJ/wiki
+	. /home/ec2-user/.nvm/nvm.sh
 	nvm install 16
+	mkdir -p logs
+	mv nohup.out logs 2>/dev/null
 	nohup node server &
 popd 
 
+sleep 3
 ps -ef | grep "node server"
 
 echo "Completed Starting Node Server....."
